d406a4a67d59d01cb55413fc092340bd572fce98
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-03-24 11:43:52 +0100

    Add project files.

---
+++ Application/DB/DocumentStoreHolder.cs
@@ -0,0 +1,32 @@
+﻿using Raven.Client.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DB
+{
+    // 'DocumentStore' is a main-entry point for client API.
+    // It is responsible for managing and establishing connections
+    // between your application and RavenDB server/cluster
+    // and is capable of working with multiple databases at once.
+    // Due to it's nature, it is recommended to have only one
+    // singleton instance per application
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    Urls = new[] { "http://localhost:8080" },
+                    Database = "Northwind"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store => LazyStore.Value;
+    }
+}
---
+++ Application/Services/Employee/EmployeeService.cs
@@ -0,0 +1,30 @@
+﻿using Application.DB;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Employee
+{
+    public class EmployeeService
+    {
+
+        public Task<ICollection<Application.Models.Employee>> GetAll()
+        {
+            using (IDocumentSession session = DocumentStoreHolder.Store.OpenSession())
+            {
+                // return all entities from 'Employees' collection
+                // where FirstName equals 'Robert'
+                ICollection<Application.Models.Employee> employees = session
+                    .Query<Models.Employee>().ToList();
+
+                return Task.FromResult(employees);
+            }
+        }
+
+        
+    }
+}
